425dee2a12a5a0bf1968e13110ec4f64a676718b
unknown <shiwanshudev@protonmail.com>
2023-10-14 22:27:01 +0530

    Add social media icon links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image";
+import { AiFillBehanceCircle, AiFillGithub, AiFillMail } from "react-icons/ai";
+import Link from "next/link";
+
+const SOCIAL_LINKS = {
+  behance: "https://www.behance.net/sshubhamdesigns",
+  github: "https://github.com/shiwanshudev",
+  mail: "mailto:shiwanshudev@outlook.com",
+};
+
 export default function Home() {
   return (
     <main className="bg-indigo-950 min-h-screen text-white">
@@ -10,6 +19,29 @@ export default function Home() {
           <p className="text-xl lg:text-2xl pt-3 font-quattrocento">
             I create web applications, user interfaces and graphic designs.
           </p>
+          <div className="mt-5 flex justify-center lg:justify-start">
+            <Link
+              className="text-white text-3xl hover:text-indigo-500 duration-200 mr-3"
+              href={SOCIAL_LINKS.behance}
+              target="_blank"
+            >
+              <AiFillBehanceCircle />
+            </Link>
+            <Link
+              className="text-white text-3xl hover:text-indigo-500 duration-200 mr-3"
+              href={SOCIAL_LINKS.github}
+              target="_blank"
+            >
+              <AiFillGithub />
+            </Link>
+            <Link
+              className="text-white text-3xl hover:text-indigo-500 duration-200"
+              href={SOCIAL_LINKS.mail}
+              target="_blank"
+            >
+              <AiFillMail />
+            </Link>
+          </div>
         </div>
         <div className="w-48 h-48 mb-10 relative lg:w-72 lg:h-72 ">
           <Image
