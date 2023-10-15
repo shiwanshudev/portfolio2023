d8b4d5f5d2d051e9fe5f0fdb4622b122ef5d7f83
unknown <shiwanshudev@protonmail.com>
2023-10-15 22:30:18 +0530

    Add navigation and update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Quattrocento } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 const quattrocento = Quattrocento({
@@ -23,8 +24,42 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} ${quattrocento.className}`}>
-        {children}
+        <main className="bg-indigo-950 min-h-screen text-white">
+          <Navbar />
+          {children}
+        </main>
       </body>
     </html>
   );
 }
+
+const Navbar = () => {
+  return (
+    <div className="items-center hidden w-80 justify-around ml-auto mr-60 pt-5 lg:flex">
+      <Link
+        href="/"
+        className="text-indigo-500 hover:text-indigo-300 duration-200 text-lg"
+      >
+        Home
+      </Link>
+      <Link
+        href="/about"
+        className="text-indigo-500 hover:text-indigo-300 duration-200 text-lg"
+      >
+        About
+      </Link>
+      <Link
+        href="/work"
+        className="text-indigo-500 hover:text-indigo-300 duration-200 text-lg"
+      >
+        Work
+      </Link>
+      <Link
+        href="/contact"
+        className="text-indigo-500 hover:text-indigo-300 duration-200 text-lg"
+      >
+        Contact Me
+      </Link>
+    </div>
+  );
+};
