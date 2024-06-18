2103350e71faa57c9c3231bab7e33a9e509ec809
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-06-18 12:44:31 +0530

    Update work with projects

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -2,36 +2,50 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function About() {
+  const WORK_LINKS = [
+    {
+      link: "https://con10tlabs.com/",
+      name: "Con10T Labs Website",
+      description:
+        "Next.js, TSParticles,Tailwind CSS, Notion API, Zoho CRM & Desk APIs",
+      src: "/images/project2.png",
+    },
+    {
+      link: "https://www.figma.com/design/2Ng2OS9TLYmpsKuDmWG7KL/Shiwanshu---UI-Portfolio?node-id=0-1&t=cjeMhVM2WvBUFEyT-1",
+      name: "Marketing Website Design",
+      description: "Figma, Prototyping, Wireframing, UX/UI Design, User Story",
+      src: "/images/project3.png",
+    },
+    {
+      link: "https://shiwanshudev.vercel.app/",
+      name: "Portfolio Website",
+      description:
+        "     NextJS, ReactJS, TailwindCSS, and incorporates the Google Sheets API.",
+      src: "/images/project1.png",
+    },
+  ];
   return (
-    <div className="mx-auto container text-center px-5 lg:px-48 lg:text-left">
+    <div className="mx-auto container text-center px-5 lg:px-48">
       <section className="pt-20">
         <h1 className="font-bold text-3xl text-center lg:text-4xl">My Work</h1>
-        <div className="pt-5 flex flex-col items-center justify-center">
-          <Link
-            href={"https://shiwanshu2023.vercel.app/"}
-            target="_blank"
-            className="outline-none"
-          >
-            <div className="flex items-center justify-center bg-purple-900 py-3 px-5 rounded-md flex-col duration-300 border-solid border border-purple-500 hover:bg-purple-800 mb-2 lg:flex-row">
-              <div className="w-full lg:w-3/12 h-52 lg:h-28 relative lg:mr-5">
-                <Image
-                  src={"/images/project1.png"}
-                  alt="Portfolio Website Project"
-                  className="border-solid border border-purple-500 rounded-md object-cover"
-                  fill={true}
-                />
-              </div>
-              <div className=" w-full lg:w-9/12">
-                <h2 className="text-lg lg:text-2xl font-bold text-purple-400 pt-3 lg:pt-0">
-                  Portfolio Website
-                </h2>
-                <p className="text-sm lg:text-base">
-                  This project utilizes NextJS, ReactJS, TailwindCSS, and
-                  incorporates the Google Sheets API.
+        <div className="pt-5 flex items-center justify-center flex-col lg:flex-row flex-wrap">
+          {WORK_LINKS.map((w) => (
+            <Link href={w.link} key={w.link} target="_blank">
+              <div className="flex items-center justify-center flex-col w-72 shadow-lg m-3 hover:scale-105 duration-200 border-solid border-purple-700 border rounded-md">
+                <div className="relative w-full h-48">
+                  <Image
+                    alt={w.name}
+                    fill={true}
+                    src={w.src}
+                    className="object-cover rounded-t-md"
+                  />
+                </div>
+                <p className="text-sm lg:text-base p-3 leading-7 bg-purple-800 text-purple-200 rounded-b-md w-full">
+                  {w.description}
                 </p>
               </div>
-            </div>
-          </Link>{" "}
+            </Link>
+          ))}
         </div>
       </section>
     </div>
