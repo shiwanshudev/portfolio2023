6663dd83839579ff6e024e37e77dd7491d3f7f96
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2025-03-21 17:22:31 +0530

    Modified projects list

--- utils/contstant.ts
+++ utils/contstant.ts
@@ -31,37 +31,37 @@ export const WORK_LINKS = [
     description:
       "Secure, no-nonsense, and simple notes app for your daily use. Developed a full-stack application that allows users to register, log in, and add notes. This project helped me understand how data flows within a given context.",
   },
-  // {
-  //   link: "https://medicine-management-tau.vercel.app/",
-  //   name: "Medicine Management",
-  //   github: "https://github.com/shiwanshudev/medicine-management",
-  //   technologies: [
-  //     "NextJS",
-  //     "TailwindCSS",
-  //     "TypeScript",
-  //     "MongoDB",
-  //     "shadcn/ui",
-  //     "ReactFormHook",
-  //   ],
-  //   src: "/images/project4.png",
-  //   description:
-  //     "This project enhances medicine management, ensuring accurate records and timely updates. It also provided valuable experience in building full-stack apps using Next.js.",
-  // },
-  // {
-  //   link: "https://aiplanet-dphi.vercel.app/",
-  //   name: "AI Planet Website (Dphi)",
-  //   github: "https://github.com/shiwanshudev/aiplanet_dphi",
-  //   technologies: [
-  //     "Next.js",
-  //     "ContextAPI",
-  //     "Tailwind CSS",
-  //     "MUI",
-  //     "TypeScript",
-  //   ],
-  //   src: "/images/project5.png",
-  //   description:
-  //     "Website Development: Developed the frontend for a hackathon website as part of an assignment from AI Planet for the frontend developer position. I integrated Context API for managing filters, search functionality, and ensuring type safety. The website is fully responsive.",
-  // },
+  {
+    link: "https://medicine-management-tau.vercel.app/",
+    name: "Medicine Management",
+    github: "https://github.com/shiwanshudev/medicine-management",
+    technologies: [
+      "NextJS",
+      "TailwindCSS",
+      "TypeScript",
+      "MongoDB",
+      "shadcn/ui",
+      "ReactFormHook",
+    ],
+    src: "/images/project4.png",
+    description:
+      "This project enhances medicine management, ensuring accurate records and timely updates. It also provided valuable experience in building full-stack apps using Next.js.",
+  },
+  {
+    link: "https://aiplanet-dphi.vercel.app/",
+    name: "AI Planet Website (Dphi)",
+    github: "https://github.com/shiwanshudev/aiplanet_dphi",
+    technologies: [
+      "Next.js",
+      "ContextAPI",
+      "Tailwind CSS",
+      "MUI",
+      "TypeScript",
+    ],
+    src: "/images/project5.png",
+    description:
+      "Website Development: Developed the frontend for a hackathon website as part of an assignment from AI Planet for the frontend developer position. I integrated Context API for managing filters, search functionality, and ensuring type safety. The website is fully responsive.",
+  },
   // {
   //   link: "https://todo-with-auth-six.vercel.app/",
   //   name: "Secure Tasks",
